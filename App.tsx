e5005175439cba9020d4263bae6f4a719a275296
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-06-25 21:56:32 -0500

    update app name

--- App.tsx
+++ App.tsx
@@ -66,9 +66,9 @@ class App extends Component<{}, AppComponentState> {
   }
 
   handleAppStateChange(nextAppState: string) {
-    if (nextAppState.match(/inactive|background/)) {
+    /*if (nextAppState.match(/inactive|background/)) {
       this.stopStream(); // Stop the stream when the app goes to the background
-    }
+    }*/
   }
 
   handleNtrStateChanged(state: string) {
